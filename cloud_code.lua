dec0667f72fa17cf956a87705512ac9ec804fab6
198013aaa-arch <198013aaa@gmail.com>
2026-01-15 13:56:22 +0800

    云函数更新 - 2026-01-15 13:56:23

--- cloud_code.lua
+++ cloud_code.lua
@@ -4,4 +4,8 @@ AlertDialog.Builder(activity)
 .setPositiveButton("确定",nil)
 .show()
 
-https://docs.github.com/rest/repos/contents#create-or-update-file-contents
+AlertDialog.Builder(activity)
+.setTitle("提示9494")
+.setMessage("云函数执行成功")
+.setPositiveButton("确定",nil)
+.show()
